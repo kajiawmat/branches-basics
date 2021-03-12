694fd906b0f436f13bc28535e366d0d1a614fc50
kajiawmat <78626689+kajiawmat@users.noreply.github.com>
2021-03-12 15:10:18 +0700

    Begin commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main()
+{
+	system("chcp 1251>nul");
+	cout<<endl<<"I will receive a massive of fibonacci numbers in various ways to find the golden ratio, to which the ratio of two closely standing numbers in this massive."<<endl;
+	return 0;
+}
---
+++ makefile
@@ -0,0 +1,9 @@
+CFLAGS =-Wall -Wextra -Werror
+all: prog
+
+prog: main.o
+	g++ main.o $(CFLAGS) -o prog.exe
+	./prog.exe
+
+main.o: main.cpp
+	g++ $(CFLAGS) -c main.cpp
